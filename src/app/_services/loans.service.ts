0dfc7cf63e5a295c38c7d9ebf0ef544462baf531
victorminerva <victorminerva.m@gmail.com>
2018-03-04 22:24:46 -0300

    Creating the register new loan method to register new loans into firebase database

--- src/app/_services/loans.service.ts
+++ src/app/_services/loans.service.ts
@@ -6,17 +6,26 @@ import { AuthService } from './auth.service';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import { User } from 'firebase/app';
+import { UserApp } from '../_models/user.model';
 import { ErrorHandler } from '../app.error-handler';
 
 @Injectable()
 export class LoansService {
 
     userUID: string;
+    userName: string;
+    userEmail: string;
+
+    usersEndPoint: any;
     userLoansEndPoint: any;
     userLoansRecentsEndPoint: any;
 
+    users: Observable<UserApp>[];
+
     constructor(private database: AngularFireDatabase, private firebaseAuth: AngularFireAuth) {
         this.userUID = firebaseAuth.auth.currentUser.uid;
+        this.userName = firebaseAuth.auth.currentUser.displayName;
+        this.userEmail = firebaseAuth.auth.currentUser.email;
     }
 
     retrieveAllUserLoans(): Observable<Loan[]> {
@@ -44,5 +53,45 @@ export class LoansService {
     markedAsReturned(loan: Loan) {
         this.userLoansEndPoint.update(loan.uid, { returned: true })
             .catch(ErrorHandler.handleError);
+    }
+
+    registerNewLoan(loan: Loan) {
+        const name = loan.favored.name;
+        const email = loan.favored.email;
+
+        if (loan.favored.phone === undefined) {
+            loan.favored.phone = '';
+        }
+
+        console.log(loan.favored.phone);
+
+        this.userLoansEndPoint.push(loan);
+        this.database.list<Loan>(`/loans/`).push(loan);
+
+        // USER-BORROWED REGISTER
+        this.usersEndPoint = this.database.list<UserApp>(`/users/`);
+        /** Restrieve all users registered */
+        this.users = this.usersEndPoint.snapshotChanges().map(users => {
+                            return users.map(a => {
+                                const data = a.payload.val();
+                                data.uid = a.key;
+                                return { ...data };
+                            });
+                        });
+
+        /** Replace the favored to Current User */
+        loan.favored.uid = '';
+        loan.favored.name = this.userName;
+        loan.favored.email = this.userEmail;
+        loan.favored.phone = '';
+
+        /** Retrieve the uid of the User that will be receive the loan */
+        this.users.forEach(users => {
+            users.forEach(user => {
+                if (email === user.email) {
+                    this.database.list<Loan>(`/user-borrowed/${user.uid}`).push(loan);
+                }
+            });
+        });
     }
 }
