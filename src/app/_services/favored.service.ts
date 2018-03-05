0667de93cb0f4c818b21399ea7a6ef049a8ab73c
victorminerva <victorminerva.m@gmail.com>
2018-03-04 22:25:48 -0300

    Creating the retrieveAllUserFavoreds to get the users registered into firebase database

--- src/app/_services/favored.service.ts
+++ src/app/_services/favored.service.ts
@@ -8,13 +8,21 @@ import { Favored } from '../_models/favored.model';
 export class FavoredService {
 
   userUID = '';
+  userFavoredsEndPoint: any;
 
   constructor(private database: AngularFireDatabase, private firebaseAuth: AngularFireAuth) {
     this.userUID = firebaseAuth.auth.currentUser.uid;
   }
 
   retrieveAllUserFavoreds(): Observable<Favored[]> {
-    return this.database.list<Favored>(`/user-favoreds/${this.userUID}`).valueChanges();
+    this.userFavoredsEndPoint = this.database.list<Favored>(`/user-favoreds/${this.userUID}`);
+    return this.database.list<Favored>(`/user-favoreds/${this.userUID}`).snapshotChanges().map(favoreds => {
+      return favoreds.map(favored => {
+          const data = favored.payload.val();
+          data.uid = favored.key;
+          return { ...data };
+      });
+    });
   }
 
 }
